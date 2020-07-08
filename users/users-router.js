bed5290d147cb0ad1c47fa11c211361a4882a87a
robertjallen <bobbidigi34@gmail.com>
2020-07-08 18:51:57 -0400

    add express-session and connect-session-knex

--- users/users-router.js
+++ users/users-router.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
 const Users = require("./users-model")
-// const restrict = require("../middleware/restrict")
+const restrict = require("../middleware/restrict")
 
 const router = express.Router()
 
-router.get("/users", async (req, res, next) => {
+router.get("/users",restrict(), async (req, res, next) => {
 	try {
 		res.json(await Users.find())
 	} catch(err) {
@@ -33,6 +33,41 @@ router.post("/register", async (req, res, next) => {
 		})
 
 		res.status(201).json(newUser)
+	} catch(err) {
+		next(err)
+	}
+})
+
+////////////////////////////
+//  AUTHENTICATE
+///////////////////////////
+router.post("/login", async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.findBy({ username }).first()
+		
+		if (!user) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+		// hash the password again and see if it matches what we have in the database
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+		// generate a new session for this user,
+		// and sends back a session ID
+		req.session.user = user
+
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
 	} catch(err) {
 		next(err)
 	}
